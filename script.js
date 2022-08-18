e8e0236323896bab7370af5a65b57fae11e50413
OSERHIRDEV <oserhir10@gmail.com>
2022-08-18 14:47:04 +0100

    Show the weather data in HTML

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/**
- * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
- */
-
 // API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
@@ -25,15 +18,14 @@ getWeatherData = (city) => {
 
 /**
  * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
  */
 searchCity = () => {
   const city = document.getElementById("city-input").value;
-  console.log(city);
+
   // CODE GOES HERE
   getWeatherData(city)
     .then((response) => {
-      console.log(response.main.temp);
+      showWeatherData(response);
     })
     .catch((err) => {
       console.log(err);
@@ -42,8 +34,12 @@ searchCity = () => {
 
 /**
  * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
+  document.getElementById("city-name").innerText = weatherData["name"];
+  document.getElementById("weather-type").innerText =
+    weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
 };
